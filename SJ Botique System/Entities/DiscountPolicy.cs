3713bfcb14cb4310d302046be8fd97cafdb4d824
M.Khushnood Saeed <khushnoodsaeed21@gmail.com>
2022-11-25 19:35:04 +0500

    Feature-ClassCode-Policy
    Policy code was structured and constructors for CardPolicy were added.

---
+++ SJ Botique System/Entities/DiscountPolicy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SJ_Botique_System.Entities
+{
+    public class DiscountPolicy:Policy
+    {
+        //Methods
+      new  public void  AddPolicy()
+        {
+
+          
+        }
+      new  public void ModifyPolicy()
+        {
+
+
+        }
+        public DiscountPolicy(int _id,string _description):base( _id, _description)
+        { 
+        
+        
+        }
+    }
+}
