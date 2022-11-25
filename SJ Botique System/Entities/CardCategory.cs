d12c5126df76e3ff239f32d02be727b62c701cad
M.Khushnood Saeed <khushnoodsaeed21@gmail.com>
2022-11-25 17:03:37 +0500

    Create CardCategory.cs
    CardCategory class was added.

---
+++ SJ Botique System/Entities/CardCategory.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SJ_Botique_System.Entities
+{
+    public class CardCategory
+    {
+        //Data Members
+
+        protected int Id;
+        protected string Description;
+
+        //Methods
+
+        public int GetId()
+        {
+            return this.Id;
+        }
+
+        public bool UpdateCategory()
+        {
+
+
+
+            return true;
+        }
+
+    }
+    public class Platinum:CardCategory
+    {
+
+        //DataMembers
+       private double DiscountPercent;
+        //Members
+        public double GetDiscountPercent()
+        {
+
+            return this.DiscountPercent;
+        
+        }
+
+
+
+    }
+    public class Gold: CardCategory
+    {
+
+        //DataMembers
+        private double DiscountPercent;
+        //Members
+        public double GetDiscountPercent()
+        {
+
+            return this.DiscountPercent;
+
+        }
+
+
+
+    }
+    public class Silver : CardCategory
+    {
+
+        //DataMembers
+        private double DiscountPercent;
+        //Members
+        public double GetDiscountPercent()
+        {
+
+            return this.DiscountPercent;
+
+        }
+
+
+
+    }
+}
