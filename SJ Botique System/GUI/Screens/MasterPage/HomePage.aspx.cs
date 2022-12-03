1ede760d55af1861289af84328eb751f9ead2f96
Abdullah4567 <gemstones321@gmail.com>
2022-12-03 19:36:19 +0500

    Fixed Code of CSS ERRORS

--- SJ Botique System/GUI/Screens/MasterPage/HomePage.aspx.cs
+++ SJ Botique System/GUI/Screens/MasterPage/HomePage.aspx.cs
@@ -1,17 +1,22 @@
 ﻿//using SJ_Botique_System.App_Start;
-using SJ_Botique_System.App_Start;
+using SJ_Botique_System.Entities;
 //using SJ_Botique_System.HelperClasses;
 using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
 using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
 
 namespace SJ_Botique_System.GUI.Screens.Master_Page
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
-
         protected void Page_Load(object sender, EventArgs e)
         {
-            //StringBuilder query = new StringBuilder();
+  //          StringBuilder query = new StringBuilder();
 
             //query.Append($"SELECT Id, Name, Description FROM ROLE");
 
