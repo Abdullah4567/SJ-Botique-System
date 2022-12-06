954c2b7c0f90540ecb0aa802e1474d3a1c439daf
Kashan Aqeel <kashanaqeel74@gmail.com>
2022-12-06 12:44:50 +0500

    Code Indentation Done

--- SJ Botique System/GUI/Screens/MasterPage/HomePage.aspx.cs
+++ SJ Botique System/GUI/Screens/MasterPage/HomePage.aspx.cs
@@ -16,62 +16,6 @@ namespace SJ_Botique_System.GUI.Screens.Master_Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            //          StringBuilder query = new StringBuilder();
-
-            //query.Append($"SELECT Id, Name, Description FROM ROLE");
-
-
-            // Code for User Profile
-            //query.Clear();
-            //query.Append($"SELECT U.Name,Age,Email,Contact,U.Address, User_Role.id as RoleId, R.Name From [User] U ");
-            //query.Append("join User_Role on U.Id=User_Role.U_ID join [Role] R on User_Role.Role_Id = R.Id where U.Id = 1");
-            //var result = DbUtility.GetDataTable(query.ToString());
-            //int RoleId = (int)result.Rows[0]["RoleId"];
-
-            //query.Clear();
-            //query.Append($"select  P.Name, P.Description from Role_Permission PR join Permission P on PR.Permission_id=P.Id where PR.Role_id= {RoleId}");
-            //result = DbUtility.GetDataTable(query.ToString());
-
-
-            // =========================================================================
-
-         
-
-            // =========================================================================
-
-            // Code for Adding Role
-            //query.Clear();
-            // query.Append($"INSERT INTO Role(name, description) values (@RoleName, @Desc)");
-            //var aa = DbUtility.InsertRecordInRoleTable(query.ToString(), "First role", "First desc");
-
-
-
-
-
-            // Code working Fine for SignUp Functionality
-            //query.Clear();
-            //query.Append($"[dbo].[sign_up]");
-            // var userId = DbUtility.InsertForSignUp(query.ToString(), "Ali", "Ali@gmail.com", "pass123", 22, "03212847563", "Block 22");
-
-            // =========================================================================
-
-            // Code working Fine for Login Functionality
-            //query.Clear();
-            //query.Append($"[dbo].[log_in]");
-            //LoginDetails CurrentUser = DbUtility.InsertForLogin(query.ToString(),"ahmad@gmail.com", "pass123");
-            // CurrentUser.RollName;
-            //CurrentUser.UserId;
-            // =========================================================================
-
-
-
-
-            //DropDownList1.DataSource = result;
-            //DropDownList1.DataTextField = "Name";
-            //DropDownList1.DataValueField = "Id";
-            //DropDownList1.DataBind();
-
-            //Session["LoginSession"] = loginDto;
 
         }
         protected void LinkButton13_Click(object sender, EventArgs e)
